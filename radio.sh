fcfe9b6c6d3a03b94b50e2d92c42f2277c9320ef
Konstantin8105 <konstantin8105@gmail.com>
2017-10-14 22:13:41 +0300

    add more radiostations

--- radio.sh
+++ radio.sh
@@ -2,10 +2,19 @@
 
 echo -e "Run radio"
 echo -e "Take radio stations from https://www.xatworld.com/radio-search"
-echo -e "1. Dubstep"
+echo -e "1. Drum and Bass"
+echo -e "2. Drum and Bass"
+echo -e "3. Dubstep"
 
-if [ "$1" = "1" ]; then
+if [ "$1" = "1" ]
+then
 	mplayer http://212.129.52.188:8000/192.mp3
+elif [ "$1" = "2" ]
+then
+	mplayer http://46.4.78.3:8156/relay
+elif [ "$1" = "3" ]
+then
+	mplayer http://188.165.3.104:8000
 else
 	echo -e "\tPlease enter the number"
 fi
