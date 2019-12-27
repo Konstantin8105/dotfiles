88b5f95fabb96b048e5862f9b4c0d32dfc63f0d8
Konstantin8105 <konstantin8105@gmail.com>
2019-12-27 16:56:47 +0300

    add fonts

--- startup.sh
+++ startup.sh
@@ -3,13 +3,18 @@
 cd $HOME
 
 ## get dotfiles
-git clone https://github.com/Konstantin8105/dotfiles.git
+echo "git clone https://github.com/Konstantin8105/dotfiles.git"
+echo "cd dotfiles"
 
 ## initialize vim
+echo "prepare vim"
 echo ":so /home/konstantin/dotfiles/vimrc.vim" > ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ## bashit
+echo "bashit"
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 
+## font
+echo "Install font"
--- vimrc.vim
+++ vimrc.vim
@@ -179,9 +179,9 @@ if has("unix")
 "	set guifont=Go\ Mono\ 9
 "	set guifont=Fira\ Mono\ 10
 "	set guifont=ProggyCleanTT\ 12
-"	set guifont=Go\ Mono\ Bold\ 11
+	set guifont=Go\ Mono\ Bold\ 11
 "	set guifont=Fira\ Mono\ Bold\ 10
-	set guifont=Ubuntu\ Mono\ Bold\ 10
+"	set guifont=Ubuntu\ Mono\ Bold\ 10
 elseif has("win32")
 	set guifont=Fira_Mono:h9:b
 endif
