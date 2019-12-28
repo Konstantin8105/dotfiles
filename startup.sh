7cc3a853517349470e16d537dcba782aa371f6f8
Konstantin8105 <konstantin8105@gmail.com>
2019-12-28 16:03:39 +0300

    startup: add bash_it

--- startup.sh
+++ startup.sh
@@ -9,4 +9,7 @@ git clone https://github.com/Konstantin8105/dotfiles.git
 echo ":so /home/konstantin/dotfiles/vimrc.vim" > ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+## bashit
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
 
